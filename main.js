053b2bd5d55db447ec88dfe617915b563432ed90
dcraven <dcravenus@gmail.com>
2014-07-31 19:45:52 -0600

    Moved event listeners to main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 var worm = new VowelWorm.instance();
+
 var audio = document.getElementById('media');
 audio.loop = true;
+
 var video = document.getElementById('vid');
 video.loop = true;
 
 var graphs_element = document.getElementById("graphs");
 
-function getUserMedia() {
-  (navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia || function(){alert('getUserMedia missing')})
-                        .apply(navigator, arguments);
-}
+document.getElementById("media_button").addEventListener("click",usemedia);
+document.getElementById("video_button").addEventListener("click",usevideo);
+document.getElementById("mic_button").addEventListener("click",usemic);
 
 function usemedia() {
   audio.style.display = 'block';
@@ -23,6 +24,17 @@ function usevideo() {
   worm.game.play(graphs_element);
 };
 
+function usemic() {
+  audio.pause();
+  audio.style.display = 'none';
+  getUserMedia({audio: true}, micSuccess, micFailure);
+};
+
+function getUserMedia() {
+  (navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia
+          || function(){alert('getUserMedia missing')}).apply(navigator, arguments);
+}
+
 function micSuccess(stream) {
   worm.setStream(stream);
   worm.game.play(graphs_element);
@@ -30,10 +42,4 @@ function micSuccess(stream) {
 
 function micFailure() {
   alert("Could not capture microphone input");
-};
-
-function usemic() {
-  audio.pause();
-  audio.style.display = 'none';
-  getUserMedia({audio: true}, micSuccess, micFailure);
 };
