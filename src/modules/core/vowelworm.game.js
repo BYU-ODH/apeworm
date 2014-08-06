ee728b1ad10d84497a6e3fc10e3a3edaa42ef9b4
dcraven <dcravenus@gmail.com>
2014-08-06 14:18:03 -0600

    Moved color function into vowelworm.game

--- src/modules/core/vowelworm.game.js
+++ src/modules/core/vowelworm.game.js
@@ -181,6 +181,23 @@ VowelWorm.Game = function( options ) {
     });
   };
 
+  //Color Functions
+  //Converts an integer representing a color to an integer representing a color 45 degrees away
+  var getNextColor = function(old_color){
+    if(typeof old_color == 'number'){
+      old_color = old_color.toString(16);
+      //Pad with 0's if necessary
+      while(old_color.length<6){
+        old_color = "0" + old_color;
+      }
+    }
+
+    old_color = tinycolor(old_color);
+    new_color = old_color.spin(45).toHex();
+    new_color = parseInt(new_color,16);
+    return new_color;
+  };
+
   // CREATE GAME
   var bgColor = options.background !== undefined ? options.background : 0xFFFFFF;
   game._stage = new PIXI.Stage(bgColor);
